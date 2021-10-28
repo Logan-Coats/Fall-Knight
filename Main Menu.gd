d286f0802f03220dc6633a79d4a5b396345ce466
LoganCoats22 <77402179+LoganCoats22@users.noreply.github.com>
2021-10-28 17:17:40 -0500

    Many changes
    Mostly changes to player

---
+++ Main Menu.gd
@@ -0,0 +1,7 @@
+extends Control
+
+
+
+
+func _on_StartGamebtn_pressed():
+	get_tree().change_scene("res://Level1.tscn")
--- Medieval Warrior Pack 2/Player.gd
+++ Medieval Warrior Pack 2/Player.gd
@@ -8,31 +8,84 @@ const JUMPHEIGHT = 200
 
 var velocity = Vector2.ZERO
 
-onready var animp = $AnimatedSprite
+onready var animp = $AnimatedSprite/AnimationPlayer
+onready var animt = $AnimatedSprite/AnimationTree
+onready var anims = $AnimatedSprite
 onready var swordhitbox = $hitboxpivot/SwordHitbox/collisionshape2d
+onready var animationState = animt.get("parameters/playback")
 
+enum{
+	MOVE,ATTACK
+}
+var state = MOVE
 
 func _ready():
 	randomize()
+	animt.active = true
+
+func _physics_process(delta):
+	match state:
+		MOVE:
+			move_state(delta)
+		ATTACK:
+			attack_state(delta)
+
+func move_state(delta):
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input_vector = input_vector.normalized()
+	
+	if input_vector != Vector2.ZERO:
+		if input_vector.x < 0:
+			anims.flip_h = true
+		else:
+			anims.flip_h = false
+		velocity = velocity.move_toward(input_vector *MAX_SPEED,ACCELERATION*FRICTION)
+	else:
+		animationState.travel("idle")
+		velocity.move_toward(Vector2.ZERO,FRICTION*delta)
+		
+		move()
+		if Input.is_action_just_pressed("attack"):
+			state = ATTACK
+			
+
+func attack_state(delta):
+	animationState.travel("attack")
+	velocity = velocity/1.25
+
+func move():
+	velocity = move_and_slide(velocity)
+
+func attack_animation_finished():
+	state = MOVE
+	velocity = Vector2.ZERO
 
 func _process(delta):
+	
 	if Input.is_action_pressed("ui_right"):
 		velocity.x = move_toward(velocity.x,MAX_SPEED,ACCELERATION*FRICTION)
-		animp.play("run") #change to animated sprite because have to flip left & right
+		anims.flip_h = false
+		if(is_on_floor()):
+			animp.play("running")
 	elif Input.is_action_pressed("ui_left"):
 		velocity.x = move_toward(velocity.x, -MAX_SPEED,ACCELERATION*FRICTION)
-		animp.play("run")
-	else: 
-		velocity.x = move_toward(velocity.x, 0,FRICTION*delta)
-		animp.play("idle")
-		
-	if Input.is_action_pressed("attack"):
+		anims.flip_h = true
+		if(is_on_floor()):
+			animp.play("running")
+	elif Input.is_action_pressed("attack") || Input.is_action_just_pressed("attack"):
 		animp.play("attack")
 		swordhitbox.disabled = false
-	
+		velocity = velocity/1.25
+	elif is_on_floor(): 
+		velocity.x = move_toward(velocity.x, 0,FRICTION*delta)
+		animp.play("idle")
+		swordhitbox.disabled = true
+		
 	if Input.is_action_pressed("jump") && is_on_floor():
-		velocity.y = -JUMPHEIGHT
 		animp.play("jump")
+		velocity.y = -JUMPHEIGHT
+		
 	if velocity.y > 0:
 		animp.play("falling")
 
