284ba86ec92c97258bc3a5766217d23652d2e3c6
LoganCoats22 <77402179+LoganCoats22@users.noreply.github.com>
2021-10-30 18:58:15 -0500

    After death
    Player can now reload current level after dying. (spawns player at beginning.)

--- Level1.gd
+++ Level1.gd
@@ -5,6 +5,20 @@ func _ready():
 
 
 func _on_Player_dead():
-	pass
-	#request restart or end. (with label 
-	#and button or mouse input.)
+	$Restart/HBoxContainer.visible = true
+	$Restart/restart.visible = true
+	$Restart/youdied.visible = true
+
+
+func _on_Button2_pressed():
+	get_tree().reload_current_scene()
+	PlayerStats.health = 4
+	$Restart/HBoxContainer.visible = false
+	$Restart/restart.visible = false
+	$Restart/youdied.visible = false
+
+
+func _on_Button_pressed():
+	$FadeINOUT/AnimationPlayer.play("fade out")
+	get_tree().quit()
+	
