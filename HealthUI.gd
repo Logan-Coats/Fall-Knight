aa91086080761fc9b6759fcc13cf9c6dd185179b
LoganCoats22 <77402179+LoganCoats22@users.noreply.github.com>
2021-10-29 20:30:17 -0500

    Health UI
    -functions for health ui
    -still need to make asset for heatlh ui

---
+++ HealthUI.gd
@@ -0,0 +1,24 @@
+extends Control
+
+var health = 4 setget set_health
+var max_health = 4 setget set_max_health
+
+onready var healthUIFull = $HealthUIFull
+onready var healthUIEmpty = $HealthUIEmpty
+
+func set_health(value):
+	health = clamp(value, 0, max_health)
+	if healthUIFull != null:
+		healthUIFull.rect_size.x = health # * size of indiv health bar.
+		
+func set_max_health(value):
+	max_health = max(value, 1)
+	self.health = min(health, max_health)
+	if healthUIEmpty:
+		healthUIEmpty.rect_size.x = max_health # * size of indiv health bar.
+
+func _ready():
+	self.max_health = PlayerStats.maxhealth
+	self.health = PlayerStats.health
+	PlayerStats.connect("health_changed",self,"set_health")
+	PlayerStats.connect("max_health_changed",self,"set_max_health")
