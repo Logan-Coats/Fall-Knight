f3dccd2c0c1495df07a312a99fc658a1c85e3c8a
LoganCoats22 <77402179+LoganCoats22@users.noreply.github.com>
2021-11-01 12:28:08 -0500

    skeleton improvements
    skeleton can move, die and take damage. animations play appropriately, all that is left is to implement attacking.

--- Enemies/Skeleton.gd
+++ Enemies/Skeleton.gd
@@ -3,7 +3,7 @@ extends KinematicBody2D
 
 const FRICTION = 500
 const ACCELERATION = 500
-const MAX_SPEED = 120
+const MAX_SPEED = 50
 const GRAVITY = 400
 
 enum{
@@ -34,16 +34,25 @@ func _physics_process(delta):
 	match state:
 		IDLE:
 			velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+			sprite.play("idle")
 			seek_player()
 		CHASE:
-			var player = playerdetectionzone.player
-			if player != null:
-				accelerate_towards_point(player.global_position,delta)
-			else: state = IDLE
+			if hurtbox.invincible == false:
+				sprite.play("walk")
+				var player = playerdetectionzone.player
+				if player != null:
+					accelerate_towards_point(player.global_position,delta)
+					#if player close to skeleton, state = attack
+				else: state = IDLE
 		ATTACK:
 			pass
+			#play attack animation, having hitbox enabled on specific frames.
+			#move sprite 12,-4 on faceright = true when false do -12,-4
+			#when animation is done, state = IDLE
+			#move sprite back to 0,0
 		DEAD:
 			pass
+			#do nothing, or when animation ends, enemy death effect
 	velocity.y += GRAVITY*delta
 	velocity = move_and_slide(velocity, Vector2.UP)
 	
@@ -56,16 +65,23 @@ func seek_player():
 func accelerate_towards_point(point,delta):
 	var direction = global_position.direction_to(point)
 	velocity = velocity.move_toward(direction * MAX_SPEED, ACCELERATION * delta)
-	sprite.flip_h = direction.x > 0
+	sprite.flip_h = direction.x < 0
 	if sprite.flip_h == true:
 		faceright = false
+	else: 
+		faceright = true
 
 func _on_hurtbox_area_entered(area):
+	sprite.play("hurt")
 	stats.health -= area.damage 
 	knockback = area.knockbackvector * 120
+	velocity.x = 0
+	velocity = move_and_slide(velocity, Vector2.UP)
+	hurtbox.start_invincibility(1)
 	#hit effect
 	#hurtbox.create_hurt_effect()
 
 func _on_Stats_nohealth():
 	sprite.play("death")
+	velocity = Vector2.ZERO
 	state = DEAD
