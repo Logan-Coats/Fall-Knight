e0c0eaf37330998fa2524294c91bfce7d729e1e8
LoganCoats22 <77402179+LoganCoats22@users.noreply.github.com>
2021-11-06 13:30:15 -0500

    Removed prototype boss, added summon
    Summon testing, making sure that when it is spawned by boss it works properly.

---
+++ Enemies/summon.gd
@@ -0,0 +1,42 @@
+extends KinematicBody2D
+
+export var acceleration = 300
+export var maxspeed = 150
+export var friction = 200
+
+onready var animp = $AnimationPlayer
+onready var playerdetectionzone = $PlayerDetectionZone
+onready var sprite = $Sprite
+
+var velocity = Vector2.ZERO
+var dead = false
+
+func _ready():
+	animp.play("spawn")
+
+func _physics_process(delta):
+	if !dead:
+		if !animp.is_playing():
+			animp.play("idle")
+		if playerdetectionzone.can_see_player():
+			var player = playerdetectionzone.player
+			if player != null:
+				accelerate_towards_point(player.global_position, delta)
+			velocity = move_and_slide(velocity, Vector2.DOWN)
+
+
+func accelerate_towards_point(point,delta):
+	var direction = global_position.direction_to(point)
+	velocity = velocity.move_toward(direction * maxspeed, acceleration * delta)
+	sprite.flip_h = direction.x > 0
+
+
+func _on_hurtbox_area_entered(area):
+	animp.play("death")
+	$hitbox/collisionshape2d.set_deferred("disabled",true)
+	dead = true
+	$Timer.start(1)
+
+
+func _on_Timer_timeout():
+	queue_free()
