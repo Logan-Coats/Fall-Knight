07db970edac521e019389a4aece47c1f92768ee9
LoganCoats22 <77402179+LoganCoats22@users.noreply.github.com>
2021-11-08 10:51:10 -0600

    Continuing boss drafting

--- Boss.gd
+++ Boss.gd
@@ -23,7 +23,35 @@ func _ready():
 	randomize()
 
 func _process(delta):
-	pass
+	match state:
+		IDLE:
+			animp.play("idle")
+			timer.start(2)
+		ATTACKL:
+			pass
+		ATTACKR:
+			pass
+		MOVEL:
+			pass
+		MOVER:
+			pass
+		SUMMON:
+			animp.play("summon")
+		SKILL:
+			animp.play("Skill")
+			timer.start(1.5)
 
 func summon():
 	pass
+	#spawn 2 summons then idle
+
+
+func _on_Timer_timeout():
+	pass
+	#use another match here to avoid 100000 if statements and multiple timers.
+	#if state=idle:
+		#if flip_h true: moveR else; moveL
+	#elif state = mover
+		#attackL
+	#elif state = moveL
+		#attackR
